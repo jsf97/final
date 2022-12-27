5cc958c1513d36f3883b26575e08963d28d20117
Juan <116412382+jsf97@users.noreply.github.com>
2022-12-27 17:38:20 -0300

    Solucionado, falta decimal pero no jodan. ORGULLO

--- js/javascript.js
+++ js/javascript.js
@@ -48,49 +48,72 @@ const bottomResult = document.querySelector('.bottom');
 
 const opResult = document.querySelector(".op-screen");
 
+const no = document.querySelector(".no")
+
 let op = '';
 
 let n1 = '';
 
 let n2 = '';
 
+let condition = false
+
+let result = '';
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', ()=> {
-    if (n2 === '') {
-      n1 += buttons[i].textContent;
-      n1 = parseInt(n1);
-      bottomResult.textContent = n1;
+    if (condition === false) {
+      n1 += buttons[i].textContent
+      n1 = parseInt(n1)
+      bottomResult.textContent = n1
     }
-
-    else if (n2 !== '') {
-      n2 += buttons[i].textContent;
-      n2 = parseInt(n2);
-      bottomResult.textContent = n2;
-    };
+    else {
+      n2 += buttons[i].textContent
+      n2 = parseInt(n2)
+      bottomResult.textContent = n2
+    }
   });
 };
 
 for (let i = 0; i < operator.length; i++) {
-  operator[i].addEventListener('click', ()=> {
-    op = operator[i].textContent;
-    opResult.textContent = op;
-    topResult.textContent = n1;
-    bottomResult.textContent = '';
-    n2 = 0;
-  });
-};
+    operator[i].addEventListener('click', ()=> {
+      if (condition === true) {
+        n1 = operate(op, n1, n2)
+        topResult.textContent = n1
+        n2 = '';
+      }
+      op = operator[i].textContent;
+      opResult.textContent = op;
+      bottomResult.textContent = ''
+      topResult.textContent = n1
+      condition = true;
+      })
+
+  };
+;
 
 equal.addEventListener('click', ()=> {
-  topResult.textContent = '';
+  topResult.textContent = ''
   n1 = operate(op, n1, n2)
-  bottomResult.textContent = n1;
+  bottomResult.textContent = n1
+  n2 = ''
+  op = ''
+  opResult.textContent = op
+  condition = false;
 });
 
 clear.addEventListener('click', ()=> {
-  op = '';
-  n1 = '';
-  n2 = '';
-  bottomResult.textContent = '';
-  topResult.textContent = ''
+ op = ''
+ n1 = ''
+ n2 = ''
+ result = ''
+ condition = false;
   opResult.textContent = '';
+  bottomResult.textContent = ''
+  topResult.textContent = ''
+
+})
+
+no.addEventListener('click', ()=> {
+  alert("no rompas las bolas y pone numeros enteros")
 })
